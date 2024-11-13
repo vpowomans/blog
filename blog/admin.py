7939d0bdee182e31bb608a85078bdc67dd217154
Iris Lizzet Urbano Ramos <iris@MBP-de-Iris.lan>
2024-11-12 23:21:13 -0600

    initial commit

---
+++ blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):  # new
+    list_display = (
+        "title",
+        "author",
+        "body",
+    )
+
+
+admin.site.register(Post, PostAdmin)  # new
+
+# Register your models here.
---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404  # new
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, "home.html", {"posts": posts})
+
+
+def post_detail(request, pk):  # new
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, "post_detail.html", {"post": post})
+
+
+# Create your views here.
